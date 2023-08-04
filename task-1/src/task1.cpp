045c9d0d155175da5a7afcb7c6fcefd26bda0b27
XimensenaS <133493894+XimensenaS@users.noreply.github.com>
2023-08-04 23:39:54 +0600

    Chapter 32

--- task-1/src/task1.cpp
+++ task-1/src/task1.cpp
@@ -1,22 +1,47 @@
 #include <iostream>
 #include <cstdlib>
-#include <sstream>
+#include <fstream>
 #include <string>
 #include <vector>
-#include "branch.h"
+#include <map>
+#include "nlohmann\json.hpp"
 
 using namespace std;
 
+struct film
+{
+    string country;
+    string datePremiere;
+    string studio;
+    string scriptwriter;
+    string director;
+    string producer;
+    map<string, string> actors;
+};
+
+
 int main()
 {
-    shared_ptr<Toy> Ball = make_shared<Toy>("Ball");
-    Dog S("Sharik", 3);
-    Dog D("Drujok");
-    S.getToy(Ball);
-    S.getToy(Ball);
-    D.getToy(Ball);
-    S.gropToy();
-    D.getToy(Ball);
-    S.gropToy();
-    Ball.reset();
+    film film;
+    film.country = "USA";
+    film.datePremiere = "05.04.23";
+    film.studio = "Universal Pictures,Illumination,Nintendo";
+    film.scriptwriter = "Matthew Vogel";
+    film.director = "Aaron Horvath, Michael Jelenic";
+    film.producer = "Christopher Meledandri,Shigeru Miyamoto";
+    film.actors["Chris Pratt"] = "Mario";
+    film.actors["Charlie Day"] = "Luigi";
+    film.actors["Anya Taylor-Joy"] = "Princess Peach";
+    film.actors["Jack Black"] = "Bowser";
+    film.actors["Keegan-Michael Key"] = "Toad";
+    ofstream file("C:/Users/User/all-tacks/task-1/src/film1.json");
+    nlohmann::json kino = {
+        {"country", film.country},
+        {"datePremiere", film.datePremiere},
+        {"studio", film.studio},
+        {"scriptwriter", film.scriptwriter},
+        {"director", film.director},
+        {"producer", film.producer},
+        {"actor", film.actors}};
+    file << kino;
 }
--- task-2/src/tack2.cpp
+++ task-2/src/tack2.cpp
@@ -1,20 +1,56 @@
 #include <iostream>
 #include <vector>
 #include <memory>
-#include "shape.h"
+#include <fstream>
+#include <string>
+#include <map>
+#include "nlohmann\json.hpp"
 
 using namespace std;
 
+struct film
+{
+    string country;
+    string datePremiere;
+    string studio;
+    string scriptwriter;
+    string director;
+    string producer;
+    map<string, string> actors;
+};
+
 int main()
 {
-    Shared_ptr_toy Ball = make_shared_toy("Ball");
-    Dog S("Sharik", 3);
-    Dog D("Drujok", 1);
-    S.getToy(Ball);
-    S.getToy(Ball);
-    D.getToy(Ball);
-    S.gropToy();
-    D.getToy(Ball);
-    S.gropToy();
-    Ball.reset();
+    int count = 5;
+    film movie[5];
+    string names[count] = {
+        "The Super Mario Bros. Movie",
+        "Guardians of the Galaxy Vol. 3",
+        "Spider-Man: Across the Spider-Verse",
+        "Dungeons & Dragons: Honor Among Thieves",
+        "Oppenheimer"};
+    ifstream file("C:/Users/User/all-tacks/task-2/src/film2.json");
+    nlohmann::json movi[5];
+    for (int i = 0; i < 5; i++)
+    {
+        file >> movi[i];
+        movie[i].country = movi[i][names[i]]["country"];
+        movie[i].datePremiere = movi[i][names[i]]["datePremiere"];
+        movie[i].studio = movi[i][names[i]]["studio"];
+        movie[i].scriptwriter = movi[i][names[i]]["scriptwriter"];
+        movie[i].director = movi[i][names[i]]["director"];
+        movie[i].producer = movi[i][names[i]]["producer"];
+        movie[i].actors = movi[i][names[i]]["actor"];
+    }
+    cout << "Enter actors: ";
+    string actor;
+    cin >> actor;
+    for (int i = 0; i < count; i++)
+    {
+        if (movie[i].actors.find(actor) != movie[i].actors.end())
+        {
+            cout <<  "movie: " << names[i] << endl << "pole: " << movie[i].actors.at(actor) << endl;
+        }
+    }
+    file.close();
 }
