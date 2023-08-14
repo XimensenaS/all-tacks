d43d3da4aa93e6c1ac5ccc3a5d74e6cb584d2066
XimensenaS <133493894+XimensenaS@users.noreply.github.com>
2023-08-14 22:41:10 +0600

    Chapter  35

--- task-1/src/task1.cpp
+++ task-1/src/task1.cpp
@@ -4,88 +4,15 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <exception>
 
 using namespace std;
 
-void addProduct(map<string, int> &m,string &n,int &c)
-{
-    if(c < 1)
-    {
-        throw invalid_argument("Count product");
-    }
-    m.insert(make_pair(n,c));
-}
-
-void removeProduct(map<string, int> &m,string &n,int &c)
-{
-    if(m.at(n) < c)
-    {
-        throw invalid_argument("Count product");
-    }
-    else if (m.count(n) == 0)
-    {
-        throw runtime_error("Name product");
-    }
-    m.erase(n);
-}
-
 int main()
 {
-    map<string, int> database;
-    map<string, int> basket;
-    string command;
-    cout << "Enter name product(or 'end' for exit): ";
-    cin >> command;
-    while(command != "end")
+    vector<int> arr = {1,2,3,4,5};
+    for(auto num : arr)
     {
-        int counting;
-        cout << "Enter count product: ";
-        cin >> counting;
-        try
-        {
-            addProduct(database,command,counting);
-        }
-        catch(const invalid_argument& e)
-        {
-            std::cerr << "Invalid argument." << endl;
-        }
-        cout << "Enter name product(or 'end' for exit): ";
-        cin >> command;
-    }
-    cout << "Enter command 'add' or 'remove' or 'exit' for exit: ";
-    cin >> command;
-    while (command != "exit")
-    {
-        string name;
-        cout << "Enter name product(or 'end' for exit): ";
-        cin >> name;
-        int counting;
-        cout << "Enter count product: ";
-        cin >> counting;
-        try
-        {
-            if (command == "add")
-            {
-                addProduct(basket,name,counting);
-                removeProduct(database,name,counting);
-            }
-            else if (command == "remove")
-            {
-                removeProduct(basket,name,counting);
-                addProduct(database,name,counting);
-            }
-             
-        }
-        catch(const invalid_argument& e)
-        {
-            cerr << "Invalid argument." << endl;
-        }
-        catch(const runtime_error& e)
-        {
-            cerr << "Not found product" << endl;
-        }
-        cout << "Enter command 'add' or 'remove' or 'exit' for exit: ";
-        cin >> command;
+        cout << num << endl;
     }
 }
+
--- task-2/src/tack2.cpp
+++ task-2/src/tack2.cpp
@@ -7,78 +7,7 @@
 #include <exception>
 using namespace std;
 
-class CatchFishException : public exception
-{
-    const char *what() const noexcept override
-    {
-        return "Catch fish";
-    }
-};
-
-class CatchTheshException : public exception
-{
-    const char *what() const noexcept override
-    {
-        return "Catch boot";
-    }
-};
-
-void fishing(string l[10], int f)
-{
-    if (f > 10)
-    {
-        throw invalid_argument("sector");
-    }
-    if (l[f - 1] == "fish")
-    {
-        throw CatchFishException();
-    }
-    else if (l[f - 1] == "thesh")
-    {
-        throw CatchTheshException();
-    }
-}
-
 int main()
 {
-    srand(time(nullptr));
-    string lake[10];
-    lake[rand() % 9] = "fish";
-    for (int i = 0; i < 3; i++)
-    {
-        int f = rand() % 9;
-        if (lake[f] != "fish")
-        {
-            lake[f] = "thesh";
-        }
-        else
-        {
-            --i;
-        }
-    }
-    int attempts = 1;
-    int slot;
-    cout << "Enter sector number: ";
-    cin >> slot;
-    bool stop = false;
-    while (!stop)
-    {
-        try
-        {
-            fishing(lake, slot);
-            cout << "Enter sector number: ";
-            cin >> slot;
-            ++attempts;
-        }
-        catch (const CatchTheshException &e)
-        {
-            std::cerr << "Unlucky fishing, you catch boot." << endl;
-            stop = true;
-        }
-        catch (const CatchFishException &e)
-        {
-            std::cerr << "Lucky,you catch fish for " << attempts << " attemps." << endl;
-            stop = true;
-        }
-    }
+    
 }
