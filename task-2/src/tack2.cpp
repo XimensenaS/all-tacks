187f8dc4c9466d4fa06c5e0c03d0a3e7f1fd604a
XimensenaS <133493894+XimensenaS@users.noreply.github.com>
2023-08-14 23:09:31 +0600

    Chapter 35

--- task-2/src/tack2.cpp
+++ task-2/src/tack2.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include <vector>
-#include <fstream>
+#include <cassert>
 #include <string>
-#include <time.h>
-#include <stdlib.h>
 #include <exception>
+#include <memory>
+#include <unordered_set>
 using namespace std;
 
 int main()
 {
-    
+    auto f = [](vector<int> vec)
+    {
+        unordered_set<int> r;
+        for (auto nums : vec)
+        {
+            if (!r.count(nums))
+            {
+                r.insert(nums);
+            }
+        }
+        auto x = make_unique<vector<int>>(r.begin(), r.end());
+        return x;
+    };
 }
